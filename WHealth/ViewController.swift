62a0173d88e6dcfb5582b2e554036cfa9f3f6d0a
Jovito Royeca <jovit.royeca@gmail.com>
2018-10-12 16:01:28 -0400

    Initial Commit

---
+++ WHealth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WHealth
+//
+//  Created by Jovito Royeca on 12.10.18.
+//  Copyright © 2018 Jovito Royeca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
